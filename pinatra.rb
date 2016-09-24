2a34105b1f448d475c05a96e8b1ff87b105a8aa5
Yoshinari Nomura <nom@nomlab.org>
2016-09-24 14:03:10 +0900

    Introduce basic Etag-based cache mechanism

--- pinatra.rb
+++ pinatra.rb
@@ -2,11 +2,12 @@ require 'sinatra'
 require './picasa_client'
 require 'json'
 
-def album_name_to_id(client, album_name)
-  album = client.album.list.entries.find do |a|
-    a.title == album_name
-  end
-  return album && album.id
+def find_album_by_name(client, album_name)
+  client.album.list.entries.find {|a| a.title == album_name}
+end
+
+def pinatra_cache_file_name(album)
+  "pinatra.#{album.id}.#{album.etag}.cache"
 end
 
 picasa_client = Pinatra::PicasaClient.new.client
@@ -17,10 +18,14 @@ end
 
 get "/:album/photos" do
   contents = []
-  album_id = album_name_to_id(picasa_client, params[:album])
-  return "Not found" unless album_id
-  album = picasa_client.album.show(album_id, {thumbsize: "128c"})
-  album.photos.each do |p|
+  album = find_album_by_name(picasa_client, params[:album])
+  return "Not found" unless album
+
+  cache_file = pinatra_cache_file_name(album)
+  return File.open(cache_file).read if File.exists?(cache_file)
+
+  photos = picasa_client.album.show(album.id, {thumbsize: "128c"}).photos
+  photos.each do |p|
     thumb = p.media.thumbnails.first
     photo = {
       src: p.content.src,
@@ -33,5 +38,10 @@ get "/:album/photos" do
     }
     contents << photo
   end
-  "pinatra_photo_list(" + contents.to_json + ");"
+  content = "pinatra_photo_list(" + contents.to_json + ");"
+
+  File.open(cache_file, "w") do |file|
+    file.print content
+  end
+  return content
 end
