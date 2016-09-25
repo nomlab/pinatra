bd14d921539e556bba1de3e844d9f402682b3e2f
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2016-09-25 13:17:10 +0900

    Accept callback parameter

--- pinatra.rb
+++ pinatra.rb
@@ -18,30 +18,42 @@ end
 
 get "/:album/photos" do
   contents = []
+  callback = params['callback']
   album = find_album_by_name(picasa_client, params[:album])
   return "Not found" unless album
 
   cache_file = pinatra_cache_file_name(album)
-  return File.open(cache_file).read if File.exists?(cache_file)
-
-  photos = picasa_client.album.show(album.id, {thumbsize: "128c"}).photos
-  photos.each do |p|
-    thumb = p.media.thumbnails.first
-    photo = {
-      src: p.content.src,
-      title: p.title,
-      thumb: {
-        url: thumb.url,
-        width: 128,
-        height: 128
+  if File.exists?(cache_file)
+    json = File.open(cache_file).read
+  else
+    photos = picasa_client.album.show(album.id, {thumbsize: "128c"}).photos
+    photos.each do |p|
+      thumb = p.media.thumbnails.first
+      photo = {
+        src: p.content.src,
+        title: p.title,
+        thumb: {
+          url: thumb.url,
+          width: 128,
+          height: 128
+        }
       }
-    }
-    contents << photo
+      contents << photo
+    end
+    json = contents.to_json
   end
-  content = "pinatra_photo_list(" + contents.to_json + ");"
+
+  if callback
+    content_type :js
+    content = "#{callback}(#{json});"
+  else
+    content_type :json
+    content = json
+  end
 
   File.open(cache_file, "w") do |file|
-    file.print content
+    file.print json
   end
+
   return content
 end
