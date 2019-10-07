ee49ce7b26d64dea4d7c3ab5806f2fe1d5632809
Ryota Nishi <nishi@swlab.cs.okayama-u.ac.jp>
2019-10-07 11:51:44 +0900

    Revert "Modify to return pinatra's unique url"
    This reverts commit a6ee97ecbc7c07b111cdfeb504c9bd89e74bd2d7.

--- pinatra.rb
+++ pinatra.rb
@@ -81,8 +81,7 @@ get "/:album_id/photos" do
       title: p["filename"],
       id: p["id"],
       thumb: {
-        #url: p["baseUrl"],
-        url: "http://seiryu.swlab.cs.okayama-u.ac.jp/#{p["id"]}",
+        url: p["baseUrl"],
         width: 128,
         height: 128
       }
@@ -102,10 +101,6 @@ get "/:album_id/photos" do
   # FIXME: アルバムに更新がなければ保存したキャッシュを使うほうがよい．
   #cache.save(album_id, json)
   return content
-end
-
-get "/photo/:mediaitem_id" do
-  return google_photo_client.get_photo(mediaitem_id).to_h["baseUrl"]
 end
 
 # Upload contents of files as image.
